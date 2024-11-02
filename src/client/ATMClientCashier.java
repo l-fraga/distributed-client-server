320d85cc746dadd582989e59eb254f5233b4e96e
Lucas Silva <lucassilvacma@hotmail.com>
2024-11-02 17:04:13 -0300

    implementation of ATMClientCashier class

--- src/client/ATMClientCashier.java
+++ src/client/ATMClientCashier.java
@@ -1,7 +1,25 @@
 package client;
 
+import interfaces.BankService;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class ATMClientCashier {
     public static void main(String[] args) {
-
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+            BankService bankService = (BankService) registry.lookup("BankService");
+            String accountId = "12345";
+            bankService.deposit(accountId, 300.0);
+            System.out.println("Depósito de 300.0 realizado na conta " + accountId);
+            bankService.withdraw(accountId, 100.0);
+            System.out.println("Saque de 100.0 realizado na conta " + accountId);
+            double saldo = bankService.consultBalance(accountId);
+            System.out.println("Saldo atual da conta " + accountId + ": " + saldo);
+        } catch (Exception e) {
+            System.out.println("Erro no cliente Caixa Automático: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
