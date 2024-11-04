93082d438a64cf91583cf51dd4c4a60418a971e3
Lucas Silva <lucassilvacma@hotmail.com>
2024-11-04 20:09:57 -0300

    prints para demonstrar execucao

--- src/tests/ConcurrencyTest.java
+++ src/tests/ConcurrencyTest.java
@@ -13,12 +13,15 @@ public class ConcurrencyTest {
             BankService bankService = (BankService) registry.lookup("BankService");
 
             String accountId = "12345";
+            System.out.println("Abrindo conta com ID: " + accountId);
             bankService.openAccount(accountId);
 
             Thread t1 = new Thread(() -> {
                 try {
                     String transactionId = UUID.randomUUID().toString();
+                    System.out.println("[T1] Iniciando depósito de 100 com transactionId: " + transactionId);
                     bankService.deposit(accountId, 100, transactionId);
+                    System.out.println("[T1] Depósito de 100 concluído para transactionId: " + transactionId);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -27,7 +30,9 @@ public class ConcurrencyTest {
             Thread t2 = new Thread(() -> {
                 try {
                     String transactionId = UUID.randomUUID().toString();
+                    System.out.println("[T2] Iniciando depósito de 200 com transactionId: " + transactionId);
                     bankService.deposit(accountId, 200, transactionId);
+                    System.out.println("[T2] Depósito de 200 concluído para transactionId: " + transactionId);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -36,12 +41,15 @@ public class ConcurrencyTest {
             Thread t3 = new Thread(() -> {
                 try {
                     String transactionId = UUID.randomUUID().toString();
+                    System.out.println("[T3] Iniciando saque de 150 com transactionId: " + transactionId);
                     bankService.withdraw(accountId, 150, transactionId);
+                    System.out.println("[T3] Saque de 150 concluído para transactionId: " + transactionId);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             });
 
+            System.out.println("Iniciando operações concorrentes...");
             t1.start();
             t2.start();
             t3.start();
